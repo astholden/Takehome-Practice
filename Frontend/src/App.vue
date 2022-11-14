fcaef7c884326d7289e84e117092cc0ddc4512af
Sam Pyo <samhpyo@gmail.com>
2022-11-13 21:00:48 -0800

    Initial webpage setup

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,3 +1,51 @@
 <template>
-  <div>Hello World</div>
+  <div class="App">
+    <h1 class="Header">Punk API Beers</h1>
+    <p>Filter beer by name</p>
+    <input type="text" v-model="beerName" placeholder="example: Hazy Jane" />
+    <div v-for="beer in modifiedList" :key="beer.id">
+      <Card :beer="beer" />
+    </div>
+  </div>
 </template>
+
+<script>
+import Card from "@/components/Card.vue";
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      beers: [],
+      modifiedList: [],
+      beerName: "",
+      listLenght: 20,
+    };
+  },
+  computed: {},
+  methods: {},
+  async created() {
+    const response = await fetch("https://api.punkapi.com/v2/beers");
+    const data = await response.json();
+    this.beers = data;
+    data.forEach((beer) => {
+      let beerObj = {
+        id: beer.id,
+        name: beer.name,
+        tagline: beer.tagline,
+        description: beer.description,
+        image_url: beer.image_url,
+        food_pairing: beer.food_pairing,
+        abv: beer.abv,
+        ibu: beer.ibu,
+        ph: beer.ph,
+      };
+      this.modifiedList.push(beerObj);
+    });
+  },
+  mounted() {},
+  unmounted() {},
+};
+</script>
---
+++ Frontend/src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ this.beer.name }}</p>
+    <p>{{ this.beer.tagline }}</p>
+    <p>{{ this.beer.description }}</p>
+    <img :src="this.beer.image_url" />
+    <p>{{ this.beer.food_pairing }}</p>
+    <p>{{ this.beer.abv }} | {{ this.beer.ibu }} | {{ this.beer.ph }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: {
+    beer: Object,
+  },
+};
+</script>
